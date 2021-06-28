4163b81a6afac96217d8199c41501387f6a74e6c
decca-dev <medcherbani@gmail.com>
2021-06-28 16:43:35 +0000

    some changes to the api so we dont accidentally leak shit

--- src/routes/api.js
+++ src/routes/api.js
@@ -15,7 +15,25 @@ router.get('/', (req, res) => {
 
 router.get('/users', validateApiKey, async (req, res) => {
     const users = await User.find()
-    res.status(200).send(users)
+    let data = [];
+    for (let i = 0; i < users.length; i++) {
+        let payload = {
+            uid: users[i].uid,
+            name: users[i].name,
+            slug: users[i].slug,
+            avatar: users[i].avatar,
+            bio: users[i].bio,
+            isBanned: users[i].isBanned,
+            isAdmin: users[i].isAdmin,
+            followers: users[i].followers,
+            following: users[i].following,
+            posRep: users[i].posRep,
+            negRep: users[i].negRep,
+            registeredAt: users[i].registeredAt
+        }
+        data.push(payload)
+    }
+    res.status(200).send(data)
 })
 
 router.get('/users/:slug', validateApiKey, async (req, res) => {
@@ -24,7 +42,21 @@ router.get('/users/:slug', validateApiKey, async (req, res) => {
         error: true,
         message: "user not found"
     })
-    res.status(200).send(user)
+    const data = {
+        uid: user.uid,
+        name: user.name,
+        slug: user.slug,
+        avatar: user.avatar,
+        bio: user.bio,
+        isBanned: user.isBanned,
+        isAdmin: user.isAdmin,
+        followers: user.followers,
+        following: user.following,
+        posRep: user.posRep,
+        negRep: user.negRep,
+        registeredAt: user.registeredAt
+    }
+    res.status(200).send(data)
 })
 
 router.get('/users/:slug/posts', validateApiKey, async (req, res) => {
@@ -34,12 +66,51 @@ router.get('/users/:slug/posts', validateApiKey, async (req, res) => {
         message: "user not found"
     })
     const posts = await Post.find({ author: user.uid });
-    res.status(200).send(posts)
+
+    let data = [];
+
+    if (posts.length > 0) {
+        for (let i = 0; i < posts.length; i++) {
+            let payload = {
+                title: posts[i].title,
+                description: posts[i].description,
+                markdown: posts[i].markdown,
+                sanitizedHTML: posts[i].sanitizedHTML,
+                slug: posts[i].slug,
+                author: posts[i].author,
+                views: posts[i].views,
+                seenBy: posts[i].seenBy
+            }
+            data.push(payload)
+        }
+    }
+
+    res.status(200).send(data)
 })
 
 router.get('/posts', validateApiKey, async (req, res) => {
     const posts =  await Post.find()
-    res.status(200).send(posts)
+
+    let data = [];
+
+    
+    if (posts.length > 0) {
+        for (let i = 0; i < posts.length; i++) {
+            let payload = {
+                title: posts[i].title,
+                description: posts[i].description,
+                markdown: posts[i].markdown,
+                sanitizedHTML: posts[i].sanitizedHTML,
+                slug: posts[i].slug,
+                author: posts[i].author,
+                views: posts[i].views,
+                seenBy: posts[i].seenBy
+            }
+            data.push(payload)
+        }
+    }
+
+    res.status(200).send(data)
 })
 
 router.get('/posts/:slug', validateApiKey, async (req, res) => {
@@ -48,7 +119,19 @@ router.get('/posts/:slug', validateApiKey, async (req, res) => {
         error: true,
         message: "post not found"
     })
-    res.status(200).send(post)
+
+    const data = {
+        title: post.title,
+        description: post.description,
+        markdown: post.markdown,
+        sanitizedHTML: post.sanitizedHTML,
+        slug: post.slug,
+        author: post.author,
+        views: post.views,
+        seenBy: post.seenBy
+    }
+
+    res.status(200).send(data)
 })
 
 module.exports = router;
