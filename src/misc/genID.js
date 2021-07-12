c6f8287b77859401d2a7b083236c5e236f82775c
decca-dev <medcherbani@gmail.com>
2021-07-12 16:08:03 +0000

    cleanup

--- src/misc/genID.js
+++ src/misc/genID.js
@@ -1,20 +1,11 @@
-const User = require('../models/User');
+module.exports = async () => {
 
-module.exports = () => {
   let id = "";
   const possible = "0123456789";
 
   for (let i = 0; i < 14; i++) {
     id += possible.charAt(Math.floor(Math.random() * possible.length));
   }
-
-  await User.findOne({ apiKey: id })
-  .then((user) => {
-    if (user) {
-      let idAsNum = parseInt(id);
-      return idAsNum + Math.floor(Math.random() * 14);
-    }else {
-      return id;
-    }
-  })
+  
+  return id;
 };
