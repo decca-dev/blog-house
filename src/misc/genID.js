087e9d73160c1436cac8882b0cd0577e29579a1b
decca-dev <medcherbani@gmail.com>
2021-07-12 16:22:37 +0000

    now it should work

--- src/misc/genID.js
+++ src/misc/genID.js
@@ -3,7 +3,7 @@ module.exports = async () => {
   let id = "";
   const possible = "0123456789";
 
-  for (let i = 0; i < 14; i++) {
+  for (let i = 0; i < Math.floor(Math.random() * 18) + 14; i++) {
     id += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   
--- src/misc/genKey.js
+++ src/misc/genKey.js
@@ -4,7 +4,7 @@ module.exports = () => {
   const possible =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for (let i = 0; i < 25; i++) {
+  for (let i = 0; i < Math.floor(Math.random() * possible.length) + 20; i++) {
     id += possible.charAt(Math.floor(Math.random() * possible.length));
   }
 
