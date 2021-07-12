2ed8bfa93139c2bbd096c8eb4d41a4c479247c8d
decca-dev <medcherbani@gmail.com>
2021-07-12 16:17:28 +0000

    commit

--- src/misc/genKey.js
+++ src/misc/genKey.js
@@ -1,4 +1,12 @@
-const { v4: uuidv4 } = require('uuid');
-module.exports = async () => {
-  return uuidv4();
+module.exports = () => {
+  let id = "";
+
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < 25; i++) {
+    id += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  return id;
 };
